159edfabc6d1a6da45bfd934cec9893c0f7bd1c7
Ahmadkhan24623-prog <khaanahmad09@gmail.com>
2026-03-03 02:22:54 +0500

    add jobs link in Netflix main page

--- Code/Footer links/Jobs/jobs.js
+++ Code/Footer links/Jobs/jobs.js
@@ -1,15 +1,36 @@
-// script.js
-
 const intro = document.getElementById("intro");
 const hero = document.getElementById("hero");
 const title = document.querySelector(".hero-title");
 const subtitle = document.querySelector(".hero-subtitle");
 
-// hide intro after 3.5 seconds
 setTimeout(() => {
   intro.style.opacity = "0";
   intro.style.visibility = "hidden";
+
+
+  hero.classList.add("active");
+
+  setTimeout(() => {
+    title.classList.add("active");
+  }, 500);
+  setTimeout(() => {
+    subtitle.classList.add("active");
+  }, 1000);
+
 }, 3500);
 
-// show hero after intro
-setTime
+
+const scrollSection = document.querySelector(".scroll-section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      scrollSection.classList.add("active");
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+observer.observe(scrollSection);
+
